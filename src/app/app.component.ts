3c0fb3cc804d65c45f0e7ea80747726ed976888c
asr <achyutsrao@gmail.com>
2018-07-29 09:24:25 +0800

    add stylings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { BarObj } from './Bar'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   bars: BarObj = {
     buttons: Array<number>(),
